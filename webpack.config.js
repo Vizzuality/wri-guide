6a3c858bf96759c8856090629819db4c30e2421b
David Inga <david.inga@vizzuality.com>
2017-07-06 20:12:51 +0200

    added publish script

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,43 @@
-'use strict';
-//  webpack.config.js
-module.exports = {
-    entry: './app/main.js',
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-    output: {
-        filename: 'wri-guide-assets.js',
-        path: __dirname
-    },
+const config = {
 
-    module: {
-      loaders: [
-        {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-        {test: /\.tpl$/, loader: 'ejs-loader'},
-        {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
-        {test: /\.(eot|ttf|woff2|woff)$/, loader: 'url-loader?prefix=fonts/&context=./app/styles/fonts'},
-      ]
-    }
+  entry: './app/main.js',
+
+  output: {
+    filename: 'wri-guide-assets.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './'
+  },
+
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.tpl$/, loader: 'ejs-loader'},
+      {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader!postcss-loader'},
+      {test: /\.(eot|ttf|woff2|woff)$/, loader: 'url-loader?prefix=fonts/&context=./app/styles/fonts'},
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+      inject: 'body',
+      filename: 'index.html'
+    })
+  ]
+
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+  config.plugins.push(new webpack.LoaderOptionsPlugin({
+    minimize: true,
+    debug: false
+  }));
+}
+
+module.exports = config;
