be930f6f8a1d24515780f0eedb3708485d71d7c7
David Inga <david.inga@vizzuality.com>
2017-07-07 10:44:01 +0200

    better webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,71 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
 
-  entry: './app/main.js',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'cheap-eval-source-map',
+
+  context: path.resolve(__dirname, 'app'),
+
+  entry: './main.js',
 
   output: {
     filename: 'wri-guide-assets.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './'
+    publicPath: '/'
   },
 
   module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      {test: /\.tpl$/, loader: 'ejs-loader'},
-      {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader!postcss-loader'},
-      {test: /\.(eot|ttf|woff2|woff)$/, loader: 'url-loader?prefix=fonts/&context=./app/styles/fonts'},
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.tpl$/,
+        loader: 'ejs-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff2|woff)$/,
+        loader: 'url-loader?prefix=fonts/&context=./app/styles/fonts'
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader?prefix=images/&context=./app/images'
+      }
     ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9090,
+    host: '0.0.0.0',
+    hot: true
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html',
+      template: 'index.html',
       inject: 'body',
       filename: 'index.html'
     })
@@ -32,12 +77,22 @@ const config = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    mangle: true,
+    sourcemap: false,
+    beautify: false,
+    dead_code: true
+  }));
   config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
   config.plugins.push(new webpack.LoaderOptionsPlugin({
     minimize: true,
     debug: false
   }));
+} else {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
 module.exports = config;
